71ba3f4471e4a942b4f46d18a1c7226940e81f71
ElideZavala <zavalavinagreelide@gmail.com>
2022-01-11 15:15:51 -0600

    results

--- app.js
+++ app.js
@@ -13,13 +13,14 @@ const app = express();
 //   res.send('You can post to this endpoint...');
 // });
 
-const x = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
-      tours: x
+      tours
     }
   });
 })
