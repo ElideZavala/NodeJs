26317035d2d88354f2581c83dd78561dca052bed
ElideZavala <zavalavinagreelide@gmail.com>
2022-01-11 15:10:31 -0600

    starus:200 success

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
-});
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const x = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours: x
+    }
+  });
+})
 
 const port = 3000;
 app.listen(port, () => {
