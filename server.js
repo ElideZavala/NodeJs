659be43862bb9f7fe0fd824019bfbf5732dca208
Elide <zavalavinagreelide@gmail.com>
2022-02-11 09:58:22 -0600

    create to new model Schema

--- server.js
+++ server.js
@@ -11,16 +11,31 @@ const DB = process.env.DATABASE.replace(
 
 // Methodo of connection.
 mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  // Conectarnos a la base de datos
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then((con) => {
-    // devolvera una promesa
-    console.log(con.connections);
-    console.log('DB connection successful!');
-  });
+  .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // Mostrara error si no se pasa este campo.
+    unique: true, // el dato debe ser unico entre todos los datos de la base de datos.
+  },
+  rating: {
+    type: Number,
+    default: 4.5, // Numero por defecto en caso de que no tenga ninguno
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
 // SERVER
 const port = process.env.PORT || 3000;
