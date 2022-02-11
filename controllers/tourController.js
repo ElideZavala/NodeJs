b36ede7dacb27790cc19c824608c5a2d741dd0e1
Elide <zavalavinagreelide@gmail.com>
2022-02-11 17:02:37 -0600

    new use async/await

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -36,13 +26,27 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+
+    // Creaacion de nuevo  objeto( Debe de contener su nuevo ID )
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    // Solicitud rechasada.
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid date sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,19 +1,17 @@
 const express = require('express');
-
-//Importamos tourController.js
 const {
   getAllTours,
   createTour,
-  checkBody,
   getTour,
   updateTour,
   deleteTour,
 } = require('../controllers/tourController');
+
 const router = express.Router();
 
 // router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
