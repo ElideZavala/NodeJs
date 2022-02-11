1dbbc401c3a6617d176bfa35402c39f2f6990166
Elide <zavalavinagreelide@gmail.com>
2022-02-10 21:07:00 -0600

    mongoose connect

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv'); // IMPORTAMOS dotenv PARA PODER USARLO.
+const app = require('./app');
+
 dotenv.config({ path: './config.env' }); // CONFIGURAMOS LA UBICACION DEL ARCHIVO CONFIG.
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Methodo of connection.
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    // devolvera una promesa
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 
 // SERVER
 const port = process.env.PORT || 3000;
