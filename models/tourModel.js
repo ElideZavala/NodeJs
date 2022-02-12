415b2a3e556041592742378c1dddf602ba7feae3
Elide <zavalavinagreelide@gmail.com>
2022-02-12 14:13:03 -0600

    new element in the model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,49 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'], // Mostrara error si no se pasa este campo.
-    unique: true, // el dato debe ser unico entre todos los datos de la base de datos.
+    unique: true, // El dato debe ser unico entre todos los datos de la base de datos.
+    trim: true, // Eliminara los espacios vacios que sean creados.
   },
-  rating: {
+  durations: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5, // Numero por defecto en caso de que no tenga ninguno
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0, // Numero por defecto en caso de que no tenga ninguno
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  // Crearemos un pequeño resumen.
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    require: [true, 'A tour must have a cover image'],
+  },
+  images: 
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
