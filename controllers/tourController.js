7c31a49a3db75dfdc8dc19434f1d5553bcaa01a4
Elide <zavalavinagreelide@gmail.com>
2022-02-11 18:05:18 -0600

    getTours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,18 +19,23 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  // const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({_id: req.params.id})
 
-  // const tour = tours.find((el) => el.id === id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      msg: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
