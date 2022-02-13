217a255b31e4ae04d5920d973c4bbfc99821df2a
Elide <zavalavinagreelide@gmail.com>
2022-02-13 12:52:04 -0600

    create a dabase for importData & deleteData

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,57 @@
+const fs = require('fs'); // Accedo al modelo de sistemas de archivo, para leer el archivo Json
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); // IMPORTAMOS dotenv PARA PODER USARLO.
+const Tour = require('./../../models/tourModel'); // Importamos el modelo
+
+dotenv.config({ path: './config.env' }); // CONFIGURAMOS LA UBICACION DEL ARCHIVO CONFIG.
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Methodo of connection.
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  // Conectarnos a la base de datos
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8') // Leemos el archivo
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create();
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany(); // Usada para elimnar muchos elementos
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv); // Importacion de guiones
