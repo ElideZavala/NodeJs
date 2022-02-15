ac5b6851ea2adb446c5bc9b98ecff0e428dd01a1
Elide <zavalavinagreelide@gmail.com>
2022-02-15 14:55:47 -0600

    create new route with new select element for find

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {
+  aliasTopTours,
   getAllTours,
   createTour,
   getTour,
@@ -10,6 +11,8 @@ const {
 const router = express.Router();
 
 // router.param('id', checkID);
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 
