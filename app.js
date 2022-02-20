2e4030d2e4c9e7bd7bf9e5d475ed4871fef1cc2e
Elide <zavalavinagreelide@gmail.com>
2022-02-19 19:13:49 -0600

    middleware of err with variable

--- app.js
+++ app.js
@@ -28,11 +28,29 @@ app.use('/api/v1/users', userRouter);
 
 // .all Se ejecuta para todos los verbos{get, post, patch, delete} * Representara que todo esta bien
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`, // Error de no podemos encontrar la pagina orginal.
+  // * Todas las URL se almacenan en el cache.
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`, // Error de no podemos encontrar la pagina orginal.
+  // });
+
+  // Utilizamos Error para establer una variable de error y poder ser utlizada en futuros errores.
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err); // El error pasara al siguiente middleware
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
-// Exporatmos app para poder utilizarlo en server
+// Exportamos app para poder utilizarlo en server
 module.exports = app;
