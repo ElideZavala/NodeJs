0dc0a2c28c9f5c8920a43ffe2956566058fdfad2
Elide <zavalavinagreelide@gmail.com>
2022-02-21 17:39:21 -0600

    error sent for developer or production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,41 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack, // Imprimimos las pilas de errores.
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR ✨', err);
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // El modulo de exportacion sera igual a esta funcion
   //   console.log(err.stack); // Nos muestra la direccion de nuestro error. // Seguimiento de la pila
-
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
--- utils/appError.js
+++ utils/appError.js
@@ -4,7 +4,7 @@ class AppError extends Error {
 
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'; // si el String empieza con 4. sera true.
-    this.isOperational = true; // Lo creamaos para probar la propiedad y solo enviar msg
+    this.isOperational = true; // Todos los errores creados seran operativos, nuestro controlador de errores.
 
     Error.captureStackTrace(this, this.constructor); // Especificamos el objeto actual y a ala clase.
   }
