87f9a78610cff181e317d4fe782953b06c031b7a
Elide <zavalavinagreelide@gmail.com>
2022-02-22 11:58:20 -0600

    message for represent the name in of err. around production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,15 @@ const handleCastErrorDB = (err) => {
   return new AppError(message);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  //   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; // Expresion regular // Mostrara los errores entre comillas
+  const value = Object.values(err.keyValue)[0]; // Del objeto de valores, se toma el nombre como primer llave valor.
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -46,6 +55,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
