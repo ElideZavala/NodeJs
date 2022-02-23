b8f1abb1a65e31ea5d55cb3eb5c7a2e47c64b377
Elide <zavalavinagreelide@gmail.com>
2022-02-23 12:49:22 -0600

    router and exports singUo used

---
+++ routes/NoteRoutes.md
@@ -0,0 +1,8 @@
+#### FILOSOFIA REST. 
+* Utilizar los metodos clasicos de `get, post, patch, delete`.
+
+#### SIN FILOSOFIA REST
+* No es necesario ningun metodoREST, Donde la *`Url sera reposable de la funcion que se realiza`*.
+``` javascript
+router.post('/signup', signup);
+// unicamente con el metodo post, ingresamos a la pagina. 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,9 +6,13 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
+
 const router = express.Router();
 
-// Route
+router.post('/signup', signup);
+
+// Router FILOSOFIA REST.
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
