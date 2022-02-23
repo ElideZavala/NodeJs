30763ff7d6ede3fdbd048596a17d744592eb2359
Elide <zavalavinagreelide@gmail.com>
2022-02-23 11:11:56 -0600

    element missing in model

--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,29 @@
 const mongoose = require('mongoose');
+const validator = require('validator'); // Validar en mongoose.
 
-// name, email. photo, password, passwordConfirm.
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A user must have a name'],
+    required: [true, 'Please tell us your name!'],
     unique: true,
   },
   email: {
     type: String,
     required: [true, 'A user must have a email'],
     unique: true,
+    lowercase: true, // Transformara el correo electronico en minusculas.
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: {
-    type: String,
-  },
+  photo: String,
   password: {
     type: String,
+    required: [true, 'Please provide a password'],
     mixlength: [8, 'A paswword must have more or equal then 8 characters'],
-    maxlength: [15, 'A paswword less have more or equal then 8 characters'],
+    // maxlength: [15, 'A paswword less have more or equal then 8 characters'],
   },
   passwordConfirm: {
     type: String,
+    required: [true, 'Please confirm your password'],
     validator: {
       validator: function (val) {
         return val === this.password;
@@ -32,3 +34,5 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+
+module.exports = User;
