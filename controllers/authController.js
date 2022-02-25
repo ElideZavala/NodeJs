c9ee849eba2d4fcc579b57afde861a9fcfd911ef
Elide <zavalavinagreelide@gmail.com>
2022-02-25 09:12:16 -0600

    generate token

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,13 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
+// Tomara el id y lo regresara de inmediato en un token.
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   // Creamos un nuevo Usuario, apartir del req.body
   const newUser = await User.create({
@@ -13,9 +20,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 
   // Generamos nuestro token con nuestro _id de mongo, la frase secreta y el tiempo en que expirara el token.
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -29,7 +34,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
-  // 1) Check if email and password exit. // Si pasamos el correo y la contraseña
+  // 1) Check if email and password exit. // Si no hay correo electronico ni contraseña obtenemos este error.
   if (!email || !password) {
     return next(new AppError('Please provide email and password!', 400)); // con return eviamos la respuesta de error
   }
@@ -37,10 +42,12 @@ exports.login = catchAsync(async (req, res, next) => {
   // 2) Check if user exits && password is correct.
   const user = await User.findOne({ email }).select('+password'); // Seleccionamos los campos requeridos.
 
-  console.log(user);
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401)); // Si el usuario existe no ejecuta ambos, pero si si existe podra executar el segundo // si el password del req.body y el password del usuario son correctos continua.
+  }
 
   // 3) If everything ok, send token to client.
-  const token = '';
+  const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token,
