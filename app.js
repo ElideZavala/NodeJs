2cb91ab91427927c8568578c9eed74bd93581300
Elide <zavalavinagreelide@gmail.com>
2022-03-14 11:03:42 -0600

    creaet file for pug

--- app.js
+++ app.js
@@ -83,6 +83,18 @@ app.get('/', (req, res) => {
   // Express buscara este archivo dentro de la carpeta que se especifico al principio.
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  }); // plantilla overview
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  }); // plantilla overview
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
