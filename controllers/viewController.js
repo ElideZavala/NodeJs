90d0a4b4774e48df1c49d2a012b99cac9926a34c
Elide <zavalavinagreelide@gmail.com>
2022-03-15 20:47:50 -0600

    tour.pug part-4

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -6,32 +6,32 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
-   // 1) Get tour data from collection
-   const tours = await Tour.find();
-   // 2) Buid template
-   // 3) Render that template using tour data from 1)
-   res.status(200).render('overview', {
-      title: 'All Tours',
-      tours,
-   });
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) Buid template
+  // 3) Render that template using tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
 });
 
 exports.getTour = catchAsync(async (req, res) => {
-   // 1) Get the data, for the requested tour (including reviews and guides)
-   // Vamos a buscar por un tour y luego vamos a incluir a reviews como path
-   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-      path: 'reviews', // ?
-      fields: 'review rating user', // Obtenemos estos campos.
-   });
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  // Vamos a buscar por un tour y luego vamos a incluir a reviews como path
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews', // ?
+    fields: 'review rating user', // Obtenemos estos campos.
+  });
 
-   //    const reviews = await Review.findOne();
-   //    const users = await User.findOne();
-   // 2) Build template
+  //    const reviews = await Review.findOne();
+  //    const users = await User.findOne();
+  // 2) Build template
 
-   // 3) Render template using data form
+  // 3) Render template using data form
 
-   res.status(200).render('tour', {
-      title: 'The Forest Hiker Tour',
-      tour,
-   });
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour,
+  });
 });
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -3,37 +3,37 @@ const mongoose = require('mongoose');
 const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
-   {
-      review: {
-         type: String,
-         required: [true, 'Review can not be empty'],
-      },
-      rating: {
-         type: Number,
-         default: 0,
-         min: 1,
-         max: 5,
-      },
-      createAt: {
-         type: Date,
-         default: Date.now,
-      },
-      tour: {
-         type: mongoose.Schema.ObjectId,
-         ref: 'Tour',
-         required: [true, 'Review must belong to a tour.'],
-      },
-      user: {
-         type: mongoose.Schema.ObjectId,
-         ref: 'User',
-         required: [true, 'review must belong to a user'],
-      },
-   },
-   {
-      // nos aseguramos que cuando tengamos una propiedad virtual , un campo no este almacenado en la base de datos, sino que se calcule usando algun valor.
-      toJSON: { virtuals: true }, // Los JSON se genera virtualmente .
-      toObject: { virtuals: true }, // Los Objetos se generan Igualmente Virtualmente.
-   }
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty'],
+    },
+    rating: {
+      type: Number,
+      default: 0,
+      min: 1,
+      max: 5,
+    },
+    createAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'review must belong to a user'],
+    },
+  },
+  {
+    // nos aseguramos que cuando tengamos una propiedad virtual , un campo no este almacenado en la base de datos, sino que se calcule usando algun valor.
+    toJSON: { virtuals: true }, // Los JSON se genera virtualmente .
+    toObject: { virtuals: true }, // Los Objetos se generan Igualmente Virtualmente.
+  }
 );
 
 reviewSchema.index({ tour: 1, user: 1 }, { unique: true }); // cada combinacion sera Unica.
@@ -43,66 +43,66 @@ reviewSchema.index({ tour: 1, user: 1 }, { unique: true }); // cada combinacion 
 
 // Vamos sobre todos los que se empiezen find.
 reviewSchema.pre(/^find/, function (next) {
-   this.populate({
-      path: 'user',
-      select: 'name photo', // En este caso solo necesitaremos el Nombre y la Foto.
-   });
+  this.populate({
+    path: 'user',
+    select: 'name photo', // En este caso solo necesitaremos el Nombre y la Foto.
+  });
 
-   next(); // continuar hacia el siguiente middlewer, de lo contrario se quedara en este.
+  next(); // continuar hacia el siguiente middlewer, de lo contrario se quedara en este.
 });
 
 // Realizamos el promedio de las calificaciones que vamos dando. // Metodo Estatico.
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
-   // Canalizacion de agregacion apuntando al modelo actual
-   const stats = await this.aggregate([
-      {
-         $match: { tour: tourId },
+  // Canalizacion de agregacion apuntando al modelo actual
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour', // Agrupamos por recorridos.
+        nRating: { $sum: 1 }, // Contara los recorridos realizados, si hay 5 documentos contara 5.
+        avgRating: { $avg: '$rating' }, // Calculamos el promedio de rating
       },
-      {
-         $group: {
-            _id: '$tour', // Agrupamos por recorridos.
-            nRating: { $sum: 1 }, // Contara los recorridos realizados, si hay 5 documentos contara 5.
-            avgRating: { $avg: '$rating' }, // Calculamos el promedio de rating
-         },
-      },
-   ]);
-   // console.log(stats);
+    },
+  ]);
+  // console.log(stats);
 
-   if (stats.length > 0) {
-      // Actualizamos todos los valores de nuestra BD de Tours.
-      await Tour.findByIdAndUpdate(tourId, {
-         ratingsQuantity: stats[0].nRating,
-         ratingsAverage: stats[0].avgRating,
-      });
-      // En caso de que no existan resultados regresamos valores predeterminados.
-   } else {
-      await Tour.findByIdAndUpdate(tourId, {
-         ratingsQuantity: 0,
-         ratingsAverage: 4.5,
-      });
-   }
+  if (stats.length > 0) {
+    // Actualizamos todos los valores de nuestra BD de Tours.
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+    // En caso de que no existan resultados regresamos valores predeterminados.
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
 // post es despues de la app, ya que todos los documentos estaran guardados, No contiene next.
 reviewSchema.post('save', function () {
-   // this point to current review
-   // El constructor es basicamente el modelo que creo ese documento, representado la gira.
-   this.constructor.calcAverageRatings(this.tour);
+  // this point to current review
+  // El constructor es basicamente el modelo que creo ese documento, representado la gira.
+  this.constructor.calcAverageRatings(this.tour);
 });
 
 // <=== Actualizar para un solo recorrido.
 // findByIdAndUpdate
 // findByIdAndDelete
 reviewSchema.pre(/^findOneAnd/, async function (next) {
-   this.revizar = await this.findOne();
-   // console.log(this.revizar);
-   next();
+  this.revizar = await this.findOne();
+  // console.log(this.revizar);
+  next();
 });
 
 // Despues de que la consulta se haya finalizado y la revision actualizado, podemos llamar calcAverageRatings
 reviewSchema.post(/^findOneAnd/, async function () {
-   // await this.findOne(); <-- does NOT work here, query has already executed.
-   await this.revizar.constructor.calcAverageRatings(this.revizar.tour);
+  // await this.findOne(); <-- does NOT work here, query has already executed.
+  await this.revizar.constructor.calcAverageRatings(this.revizar.tour);
 });
 
 // Pasar datos del middleware previo al middleware posterior.
