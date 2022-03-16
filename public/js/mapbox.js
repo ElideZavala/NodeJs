f61c37645188b0a17e82bb5fb315ef0269e4b635
Elide <zavalavinagreelide@gmail.com>
2022-03-16 13:04:32 -0600

    solve problem for install mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations); // Leemos los datos de data-locations en Pug.
 console.log(locations);
-console.table(locations);
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiZWxkZXphdmFsYSIsImEiOiJja25kcXlla2UxbHRuMm9wamhkOGVrdzZtIn0.9v_hf7Wox73rxt3HeFIX8A';
+const tokenPlaceHolder =
+  'pk.eyJ1IjoiZWxkZXphdmFsYSIsImEiOiJjbDB0d3RsNjEwcnV5M2pvMnZ4dHR6Mmk2In0.T_TrymVuHSQmLyHxt7KaIA';
+
+mapboxgl.accessToken = tokenPlaceHolder;
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
+  //   style: 'mapbox://styles/eldezavala/cl0ttrbpm000l14oqp1opuquu',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-118.113491, 34.1117545], // first longitude after latitude
+  zoom: 10,
+  interactive: false,
 });
