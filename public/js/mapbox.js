ce08f3a3c4a5b9d4df70c84559a5a282143ff26c
Elide <zavalavinagreelide@gmail.com>
2022-03-16 20:24:36 -0600

    mapbox style for parafho

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -10,6 +10,7 @@ var map = new mapboxgl.Map({
   container: 'map',
   //   style: 'mapbox://styles/eldezavala/cl0ttrbpm000l14oqp1opuquu',
   style: 'mapbox://styles/mapbox/light-v10',
+  scrollZoom: false,
   //   center: [-118.113491, 34.1117545], // first longitude after latitude
   //   zoom: 10,
   //   interactive: false,
@@ -29,8 +30,16 @@ locations.forEach((loc) => {
     element: el,
     anchor: 'bottom', // En la parte inferior del elemento se ubicara la ubicacion exacta del GPS.
   })
-    .setLngLat(loc.coordinates) // configuramos la latitud y longitud de estas coordenadas.
-    .addTo(map); // agregamos nuestra variable de map.
+    .setLngLat(loc.coordinates) // <-- configuramos la latitud y longitud de estas coordenadas.
+    .addTo(map); // <-- agregamos nuestra variable de map.
+
+  // Add popup // <-- Colocamos un parrafo en las coordenadas.
+  new mapboxgl.Popup({
+    offset: 30, // <-- Desplazamiento del parrafo.
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`) // <-- Colocamos un parrafo con la descripcion.
+    .addTo(map);
 
   // Extend map bounds to include current location. // Extendemos los limites con nuestras coordenadas.
   bounds.extend(loc.coordinates);
@@ -40,7 +49,7 @@ locations.forEach((loc) => {
 map.fitBounds(bounds, {
   padding: {
     top: 200,
-    bottom: 200,
+    bottom: 150,
     left: 100,
     right: 100,
   },
