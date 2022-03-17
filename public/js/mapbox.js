e941590a791dff404268b7752925e3dc7cc1edca
Elide <zavalavinagreelide@gmail.com>
2022-03-16 19:33:32 -0600

    ubication pound & margen

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -10,7 +10,38 @@ var map = new mapboxgl.Map({
   container: 'map',
   //   style: 'mapbox://styles/eldezavala/cl0ttrbpm000l14oqp1opuquu',
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [-118.113491, 34.1117545], // first longitude after latitude
-  zoom: 10,
-  interactive: false,
+  //   center: [-118.113491, 34.1117545], // first longitude after latitude
+  //   zoom: 10,
+  //   interactive: false,
+});
+
+// Area que se mostrara en el mapa
+const bounds = new mapboxgl.LngLatBounds();
+
+// Creamos un nuevo elemento HTML
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom', // En la parte inferior del elemento se ubicara la ubicacion exacta del GPS.
+  })
+    .setLngLat(loc.coordinates) // configuramos la latitud y longitud de estas coordenadas.
+    .addTo(map); // agregamos nuestra variable de map.
+
+  // Extend map bounds to include current location. // Extendemos los limites con nuestras coordenadas.
+  bounds.extend(loc.coordinates);
+});
+
+// Ajustar a los limites // Agregamos margen a nuestras ubicaciones.
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 200,
+    left: 100,
+    right: 100,
+  },
 });
