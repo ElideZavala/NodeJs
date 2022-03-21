fbfe79ae9a5b9912aab2b5db71a6df86c068db2d
Elide <zavalavinagreelide@gmail.com>
2022-03-20 22:40:46 -0600

    create reset textContent password and setTime of Updating

--- public/js/index.js
+++ public/js/index.js
@@ -41,8 +41,10 @@ if (userDataForm)
   });
 
 if (userPasswordForm)
-  userPasswordForm.addEventListener('submit', (e) => {
+  userPasswordForm.addEventListener('submit', async (e) => {
     e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
     const passwordCurrent = document.getElementById('password-current').value;
     const password = document.getElementById('password').value;
     const passwordConfirm = document.getElementById('password-confirm').value;
@@ -52,4 +54,9 @@ if (userPasswordForm)
       { passwordCurrent, password, passwordConfirm },
       'password'
     );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
