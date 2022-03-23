8089b3177ab527402671b43cb16e032df1cf4564
Elide <zavalavinagreelide@gmail.com>
2022-03-23 16:50:25 -0600

    create template for sendEmail and welcome

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 // Tomara el id y lo regresara de inmediato en un token.
 const signToken = (id) => {
@@ -52,6 +52,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordResetToken: req.body.passwordResetToken,
     passwordResetExpires: req.body.passwordResetExpires,
   });
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
   // Generamos nuestro token con nuestro _id de mongo, la frase secreta y el tiempo en que expirara el token.
@@ -197,11 +200,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot yout password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
