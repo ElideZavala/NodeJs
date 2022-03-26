32b4f7bfb4b624bf3955a1f583933faca4ee27ea
Elide <zavalavinagreelide@gmail.com>
2022-03-25 20:33:54 -0600

    createBookingCheckpot

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const Stripe = require('stripe');
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 // const factory = require('./handlerFactory');
 // const AppError = require('../utils/appError');
@@ -12,7 +13,9 @@ exports.getChekoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'], // Metodo aceptado
-    success_url: `${req.protocol}://${req.get('host')}/`, // pagina ala que sera direcionado nuestro usuario.
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`, // pagina ala que sera direcionado nuestro usuario.
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`, // ( pagina a la que el usuario decide cancelar el pago )
     customer_email: req.user.email, // Especificar el correo del CLIENTE, es decir de nuestro usuario.
     client_reference_id: req.params.tourId, // tomamos una referencia al tour comprado del cliente.
@@ -35,4 +38,15 @@ exports.getChekoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's a UNSECURE: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next(); // En caso de que no haya usuario, tour y presio, continuar a la siguiente.
+  await Booking.create({ tour, user, price }); // De nuestro modelo creamos estos Elementos.
+
+  //    res.redirect(${req.protocol}://${req.get('host')}); // Redireccionamos el sitio web
+  res.redirect(req.originalUrl.split('?')[0]); // Separamos la URL por ? y seleccionamos el primer valor.
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,12 +7,13 @@ const {
   updateUserData,
 } = require('../controllers/viewController');
 const { protect, isLoggedIn } = require('../controllers/authController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
 // router.use();
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
