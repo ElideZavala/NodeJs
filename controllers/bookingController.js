5f192de62aeda633d03c34de9f98ae0db19534b0
Elide <zavalavinagreelide@gmail.com>
2022-03-31 11:18:48 -0600

    Improved stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const Stripe = require('stripe');
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
@@ -50,6 +51,13 @@ exports.getChekoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]); // Separamos la URL por ? y seleccionamos el primer valor.
 // });
 
+const createBookingCheckot = async (session) => {
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email })).id; // Accedemos solo al id
+  const price = session.line_items[0].amount / 100; // lo convertimos nuevamente a dolares.
+  await Booking.create({ tour, user, price });
+};
+
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature']; // agregamos un encabezado a esta solicitud que contiene un firma especial.
 
@@ -65,7 +73,11 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
-  if(event.type === 'checkout.session.completed')
+  if (event.type === 'checkout.session.completed') {
+    createBookingCheckout(event.data.object); // Mandamos nuestro evento.
+
+    res.status(200).json({ received: true }); // Colocar como recibido.
+  }
 };
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
